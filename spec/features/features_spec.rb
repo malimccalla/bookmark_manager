c54fb1bbd24b58d4ebb09f9fa4e9e687146e3ffd
Mali Michael <malimccalla@gmail.com>
2016-07-26 10:40:03 +0100

    Create link model

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'display links' do
+  scenario 'it displays links' do
+    Link.create(url: 'http://www.bcc.co.uk/news', name: 'What is this???')
+    visit '/links'
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('What is this???')
+    end
+  end
+end
