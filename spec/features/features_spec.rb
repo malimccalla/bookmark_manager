832858b514919ee345347d33050d451b2b954a9b
lukew244 <luke.webster86@gmail.com>
2016-07-26 14:29:05 +0100

    Separates test and development envs

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,25 +1,39 @@
-require 'spec_helper'
+require 'database_cleaner'
 
-feature 'display links' do
-  scenario 'it displays links' do
-    Link.create(url: 'http://www.bcc.co.uk/news', title: 'What is this???')
-    visit '/links'
-    expect(page.status_code).to eq 200
+#DatabaseCleaner.strategy = :transaction
 
-    within 'ul#links' do
-      expect(page).to have_content('What is this???')
+
+#DatabaseCleaner.cleaning do
+  feature 'display links' do
+    scenario 'it displays links' do
+      Link.create(url: 'http://www.bcc.co.uk/news', title: 'What is this???')
+      visit '/links'
+      expect(page.status_code).to eq 200
+
+      within 'ul#links' do
+        expect(page).to have_content('What is this???')
+      end
     end
   end
+
+  feature 'add a link' do
+    scenario 'user can create links' do
+
+      visit '/links/new'
+      fill_in :title, with: 'Facebook'
+      fill_in :url, with: 'http://www.facebook.com'
+      click_button 'submit'
+      within 'ul#links' do
+        expect(page).to have_content('Facebook')
+      end
+    end
+
+
+#  end
+
+
+
 end
 
-feature 'add a link' do
-  scenario 'user can create links' do
-    visit '/links/new'
-    fill_in :title, with: 'Facebook'
-    fill_in :url, with: 'http://www.facebook.com'
-    click_button 'submit'
-    within 'ul#links' do
-      expect(page).to have_content('Facebook')
-    end
-  end
-end
+
+#DatabaseCleaner.clean
