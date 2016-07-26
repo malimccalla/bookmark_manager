5fbed8a7158ad619190d7c25a803192e1677c949
Mali Michael <malimccalla@gmail.com>
2016-07-26 18:54:27 +0100

    Extract mapper setup

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,39 +1,41 @@
 require 'database_cleaner'
 
-#DatabaseCleaner.strategy = :transaction
+feature 'display links' do
+  scenario 'it displays links' do
+    Link.create(url: 'http://www.bcc.co.uk/news', title: 'What is this???')
+    visit '/links'
+    expect(page.status_code).to eq 200
 
-
-#DatabaseCleaner.cleaning do
-  feature 'display links' do
-    scenario 'it displays links' do
-      Link.create(url: 'http://www.bcc.co.uk/news', title: 'What is this???')
-      visit '/links'
-      expect(page.status_code).to eq 200
-
-      within 'ul#links' do
-        expect(page).to have_content('What is this???')
-      end
+    within 'ul#links' do
+      expect(page).to have_content('What is this???')
     end
   end
-
-  feature 'add a link' do
-    scenario 'user can create links' do
-
-      visit '/links/new'
-      fill_in :title, with: 'Facebook'
-      fill_in :url, with: 'http://www.facebook.com'
-      click_button 'submit'
-      within 'ul#links' do
-        expect(page).to have_content('Facebook')
-      end
-    end
-
-
-#  end
-
-
-
 end
 
+feature 'add a link' do
+  scenario 'user can create links' do
 
-#DatabaseCleaner.clean
+    visit '/links/new'
+    fill_in :title, with: 'Facebook'
+    fill_in :url, with: 'http://www.facebook.com'
+    click_button 'submit'
+    within 'ul#links' do
+      expect(page).to have_content('Facebook')
+    end
+  end
+end
+
+feature 'add a tag' do
+  scenario 'user adds tag to link' do
+    visit '/links/new'
+    fill_in :title, with: 'Facebook'
+    fill_in :url, with: 'http://www.facebook.com'
+    fill_in :tags, with: 'social'
+    click_button 'submit'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('social')
+    # within 'ul#links' do
+    #   expect(page).to have_content('social')
+    # end
+  end
+end
