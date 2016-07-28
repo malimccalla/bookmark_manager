ff922ca959e474b921495ba10648217f435a6581
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-07-28 10:26:27 +0100

    add multiple tag function

--- spec/features/creating_tags_spec.rb
+++ spec/features/creating_tags_spec.rb
@@ -13,3 +13,15 @@ feature 'adding a single tag to a link' do
     expect(link.tags.map(&:name)).to include 'game'
   end
 end
+
+feature 'adding well loads of tags' do
+  scenario 'user adds loads of tags to one link' do
+    visit '/links/new'
+    fill_in 'url', with: 'rckpprscslzdspk.herokuapp.com'
+    fill_in 'title', with: 'Rock Paper Scissors Lizard Spock'
+    fill_in 'tags', with: 'game fun'
+    click_button 'Add Link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('game', 'fun')
+  end
+end
